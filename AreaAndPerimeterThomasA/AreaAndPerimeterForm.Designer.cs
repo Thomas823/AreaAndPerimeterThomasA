9214ba6ca6ed6a9028f99d68f6309e2b15bb7def
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2018-04-05 16:41:12 -0400

    Final

--- AreaAndPerimeterThomasA/AreaAndPerimeterForm.Designer.cs
+++ AreaAndPerimeterThomasA/AreaAndPerimeterForm.Designer.cs
@@ -28,20 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.lblAreaAnswer = new System.Windows.Forms.Label();
+            this.lblPerimeterAnswer = new System.Windows.Forms.Label();
+            this.btnAnswer = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrompt.Location = new System.Drawing.Point(31, 40);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(952, 31);
+            this.lblPrompt.TabIndex = 0;
+            this.lblPrompt.Text = "What is the area and perimeter of a rectangle with length 5cm and width 3 cm?";
+            // 
+            // lblAreaAnswer
+            // 
+            this.lblAreaAnswer.AutoSize = true;
+            this.lblAreaAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAreaAnswer.Location = new System.Drawing.Point(71, 135);
+            this.lblAreaAnswer.Name = "lblAreaAnswer";
+            this.lblAreaAnswer.Size = new System.Drawing.Size(0, 24);
+            this.lblAreaAnswer.TabIndex = 1;
+            // 
+            // lblPerimeterAnswer
+            // 
+            this.lblPerimeterAnswer.AutoSize = true;
+            this.lblPerimeterAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPerimeterAnswer.Location = new System.Drawing.Point(72, 254);
+            this.lblPerimeterAnswer.Name = "lblPerimeterAnswer";
+            this.lblPerimeterAnswer.Size = new System.Drawing.Size(0, 24);
+            this.lblPerimeterAnswer.TabIndex = 2;
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnswer.Location = new System.Drawing.Point(724, 233);
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.Size = new System.Drawing.Size(107, 48);
+            this.btnAnswer.TabIndex = 3;
+            this.btnAnswer.Text = "Answer";
+            this.btnAnswer.UseVisualStyleBackColor = true;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
             // 
             // frmAreaAndPerimeter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(995, 332);
+            this.Controls.Add(this.btnAnswer);
+            this.Controls.Add(this.lblPerimeterAnswer);
+            this.Controls.Add(this.lblAreaAnswer);
+            this.Controls.Add(this.lblPrompt);
             this.Name = "frmAreaAndPerimeter";
             this.Text = "Area And Perimeter By Thomas Aubin";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.Label lblAreaAnswer;
+        private System.Windows.Forms.Label lblPerimeterAnswer;
+        private System.Windows.Forms.Button btnAnswer;
     }
 }
 
--- AreaAndPerimeterThomasA/AreaAndPerimeterForm.cs
+++ AreaAndPerimeterThomasA/AreaAndPerimeterForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Thomas Aubin
+ * Created on: 5 April, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #6 - Area and Perimeter
+ * This program calculates the area and perimeter of a rectangle and displays the result 
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +23,13 @@ namespace AreaAndPerimeterThomasA
         public frmAreaAndPerimeter()
         {
             InitializeComponent();
+        }
+
+        // Calculate and display the area and perimeter of the rectangle of 5 by 3 cm
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            lblAreaAnswer.Text = "Area = " + Convert.ToString(5 * 3) + "cm";
+            lblPerimeterAnswer.Text = "Perimeter = " + Convert.ToString((2 * 5) + (2 * 3)) + "cm";
         }
     }
 }
